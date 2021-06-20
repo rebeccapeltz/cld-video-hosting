9498b46637abaa93b79fd7d8899e4bf1ce19f544
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-06-19 19:50:14 -0700

    email experiment

---
+++ functions/send-email.js
@@ -0,0 +1,22 @@
+exports.handler = function (event, context, callback) {
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  require("dotenv").config();
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "rebeccapeltz@gmail.com", // Change to your recipient
+    from: "rebeccapeltz@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
